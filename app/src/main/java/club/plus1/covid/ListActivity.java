5c01e077d4b01dec369a3b01f0f017944fffee84
lavsexpert <lavrov-sergey@yandex.ru>
2020-08-13 17:43:49 +0300

    init

---
+++ app/src/main/java/club/plus1/covid/ListActivity.java
@@ -0,0 +1,14 @@
+package club.plus1.covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+    }
+}
